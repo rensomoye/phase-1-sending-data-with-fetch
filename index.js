f7baf414c59454597cca0c0e35981ea1606d223c
Reni Somoye <rensomoye@gmail.com>
2023-02-09 00:25:51 -0800

    commit tree e16baff4722a7c9c48ab577ce844c71990f417fd

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Add your code here
+function submitData(name, email) {
+
+    const formData = {
+        name,
+        email
+    }
+    
+    const configurationObject = {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        },
+        body: JSON.stringify(formData)
+    }
+
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(res => res.json())
+    .then(data => {
+        document.body.innerHTML = `<p>${data.id}</p>`
+    })
+    .catch(error => {
+        document.body.innerHTML = `<p>${error.message}</p>`
+    })
+   
+}
+
+
+
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   };
+  
+//   // method: "POST" is missing from the object below
+//   const configurationObject = {
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (object) {
+//       console.log(object);
+//     })
+//     .catch(function (error) {
+//       alert("Bad things! Ragnarők!");
+//       console.log(error.message);
+//     });
